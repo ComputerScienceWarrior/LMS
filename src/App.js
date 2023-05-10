7deaa99715315dd3994a8a502919ba957d689b2b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-09 23:22:04 -0700

    updates routing structure to include '/about_us' route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about_us" element={<AboutUs />} />
       </Routes>
     </div>
   );
