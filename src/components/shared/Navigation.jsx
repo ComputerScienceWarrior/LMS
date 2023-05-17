5544dccc9a5d090b84e71a8c8e198d061245172b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-16 17:58:55 -0700

    update <nav/> to <Box/>

--- src/components/shared/Navigation.jsx
+++ src/components/shared/Navigation.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import { Box } from '@chakra-ui/react'
 
 const Navigation = (props) => {
     return(
-        <nav style={{display: 'flex', flexDirection: 'column', textAlign: 'center' ,backgroundColor: 'red', paddingTop: '3px', paddingBottom: '3px'}}>
+        <Box display={'flex'} flexDirection={'column'} textAlign={'center'} backgroundColor={'red'} paddingTop={'3'} paddingBottom={'3'}>
             <ul className="" style={{display: 'flex', justifyContent: 'space-evenly', listStyleType: 'none'}}>
                 <li><Link style={{textDecoration: 'none', textColor: 'white'}} to="/">Home</Link></li>
                 <li><Link style={{textDecoration: 'none', textColor: 'white'}} to="/about_us">About Us</Link></li>
             </ul>
-        </nav>
+        </Box>
     )
 }
 
