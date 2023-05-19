b7e045f8a2fcef7fca1a5425338aa730c2215167
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-19 06:54:24 -0700

    creates signup component

---
+++ src/components/Session/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Box } from '@chakra-ui/react'
+
+const SignUp = (props) => {
+
+    return(
+        <Box>
+            Here is The Sign Up Page
+        </Box>
+    )
+}
+
+export default SignUp;
