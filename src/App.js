93d78bba4482c6b02bf1131dfb8ffad7347e8fde
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-19 06:54:47 -0700

    add /sign_up route to navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
+import SignUp from './components/Session/SignUp/SignUp';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/sign_up" element={<SignUp />} />
       </Routes>
     </div>
   );
